9b33aa5965709e3b31def0a1f3ed7d0aa145d6c5
reachvenkyv <reachvenkyv@gmail.com>
2019-07-26 16:32:53 +1000

    Appagain commit

---
+++ src/main/java/mygroup/parent/AppAgain.java
@@ -0,0 +1,13 @@
+package mygroup.parent;
+
+/**
+ * Hello world!
+ *
+ */
+public class AppAgain 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World App Again in mygroup.parent!" );
+    }
+}
